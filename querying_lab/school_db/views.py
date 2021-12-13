08c2ed035fc95b1ed72a3ff5c95d2597e4c345ac
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2021-12-13 13:59:35 -0600

    "Problem three solutions"

--- querying_lab/school_db/views.py
+++ querying_lab/school_db/views.py
@@ -37,9 +37,9 @@ def problem_two(request):
 def problem_three(request):
     # Find all students who have a A+ in any class and are NOT getting a C+ in any class. 
     # Order the data by student's first name alphabetically.
-
+    student_courses = StudentCourse.objects.filter(grade='A+').order_by('-student_id')
     context = {
-        'student_courses': None
+        'student_courses': student_courses
     }
     return render(request, 'school/three.html', context)
 
