18e20034f3e7511c15dc90fdcc332875a581f38c
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2021-12-13 15:32:36 -0600

    fixing html three to match the instructions

--- querying_lab/school_db/views.py
+++ querying_lab/school_db/views.py
@@ -37,9 +37,10 @@ def problem_two(request):
 def problem_three(request):
     # Find all students who have a A+ in any class and are NOT getting a C+ in any class. 
     # Order the data by student's first name alphabetically.
-    student_courses = StudentCourse.objects.filter(grade='A+').order_by('-student')
+    student_courses = StudentCourse.objects.filter(grade='A+').order_by('student')
+    exception = student_courses.exclude(grade='C+')
     context = {
-        'student_courses': student_courses
+        'student_courses': exception
     }
     return render(request, 'school/three.html', context)
 
